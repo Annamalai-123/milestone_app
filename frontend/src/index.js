91f2ae6d94b211547f193c31aa81284adf19b5f3
annamalai <ramanannamalai2019@gmail.com>
2024-12-20 02:28:11 +0530

    build setup1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,7 @@ import { store } from './app/store';
 import App from './App';
 
 import './index.css'
-
-
-
-
+//deploy
 const container = document.getElementById('root');
 const root = createRoot(container);
 
